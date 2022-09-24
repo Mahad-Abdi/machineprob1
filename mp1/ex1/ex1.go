01f7b697e1df6566afa0fcc01577614a09d638d6
Mahad Abdi <mahad@mahads-mbp-2.bc.edu>
2022-09-24 18:23:31 -0400

    Forgot to commit ex1.go

--- mp1/ex1/ex1.go
+++ mp1/ex1/ex1.go
@@ -3,34 +3,89 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func main() {
+
 	arguments := os.Args
 	if len(arguments) == 1 {
-		fmt.Println("Please provide port number")
+		fmt.Println("Please provide line number of config file")
 		return
 	}
-	if len(arguments) == 2 {
-		fmt.Println("Please provide host:port.")
-		return
-	}
-
-	PORT := ":" + arguments[1]
-	CONNECT := arguments[2]
+	lineNumber, _ := strconv.Atoi(arguments[1])
+	configLine := readConfig(lineNumber)
+	configLineParsed := parseLine(configLine)
+	//id := configLineParsed[0]
+	//ip := configLineParsed[1]
+	port := ":" + configLineParsed[2]
+	println(port)
 
 	// I'm trying to combine the TCPServer and TCPClient into one file, he said that they both can be in the same file
-	go createTCPServer(PORT)
+	go createTCPServer(port)
+	println("Please provide a command in the form send destination message or STOP to stop proccess")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		textParsed := parseLine(text)
+		//destination := textParsed[1]
+		if len(textParsed) < 3 {
+			println("Please provide a command in the form send destination message or STOP to stop proccess")
+			return
+		}
+		// add error catching later
+		destination, _ := strconv.Atoi(textParsed[1])
+		messageReceived := textParsed[2]
+		if len(arguments) > 2 {
+			for i := 3; i < len(arguments); i++ {
+				messageReceived = messageReceived + " " + textParsed[i]
+			}
 
-	go createTCPClient(CONNECT)
+		}
+		unicastSend(destination, messageReceived)
+
+	}
+	//go createTCPClient(ip)
 
 	// Temporary
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 1000)
 
+}
+
+// Lines 40-46 & 52-54 from https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
+func readConfig(line int) string {
+	file, err := os.Open("config.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	currentLineNum := 0
+	str := ""
+	for scanner.Scan() {
+		if line == currentLineNum {
+			str = string(scanner.Text())
+			return str
+		}
+		currentLineNum++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return str
+}
+
+func parseLine(line string) []string {
+	return strings.Split(line, " ")
 }
 
 /*
@@ -53,6 +108,7 @@ func createTCPServer(PORT string) {
 	}
 
 	for {
+
 		netData, err := bufio.NewReader(c).ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
@@ -76,30 +132,42 @@ func createTCPServer(PORT string) {
 
 https://www.linode.com/docs/guides/developing-udp-and-tcp-clients-and-servers-in-go/
 */
-func createTCPClient(CONNECT string) {
+func createTCPClient(CONNECT string, message string) {
 	c, err := net.Dial("tcp", CONNECT)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
+	fmt.Println("Dialed", CONNECT)
+	reader := bufio.NewReader(os.Stdin)
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(">> ")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(c, text+"\n")
-
-		message, _ := bufio.NewReader(c).ReadString('\n')
-		fmt.Print("->: " + message)
-		if strings.TrimSpace(string(text)) == "STOP" {
+		_, err := fmt.Fprintf(c, message+"\n")
+		if err != nil {
+			fmt.Println("Error with client", err)
+			return
+		}
+		fmt.Println("Sent ", message)
+		if strings.TrimSpace(string(message)) == "STOP" {
 			fmt.Println("TCP client exiting...")
 			return
 		}
+		message, _ = bufio.NewReader(c).ReadString('\n')
+		fmt.Println("->: Received response from ", message)
+		fmt.Print(">> ")
+		message, _ = reader.ReadString('\n')
+
 	}
 
 }
 
-func unicastSend(destination string, message string) {
+func unicastSend(destination int, message string) {
+	configLine := readConfig(destination)
+	configLineParsed := parseLine(configLine)
+	//id := configLineParsed[0]
+	//ip := configLineParsed[1]
+	ip := ":" + configLineParsed[2]
+	fmt.Println("Connecting to destination", destination, ip)
+	createTCPClient(ip, message)
 
 }
 
